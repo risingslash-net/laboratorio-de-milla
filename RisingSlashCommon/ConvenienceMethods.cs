f3646884d81e52849a9f47545e784982c80d6125
Ann <ann@poppingspree.dev>
2022-10-28 23:17:48 -0500

    Add more logging convenience methods for use in static methods.

--- RisingSlashCommon/ConvenienceMethods.cs
+++ RisingSlashCommon/ConvenienceMethods.cs
@@ -11,4 +11,14 @@ public static class ConvenienceMethods
     {
         LocalLog.LogError($"{MyPluginInfo.PLUGIN_GUID} Is logging an exception:\r\n{e.Message}\r\n{e.StackTrace}\r\n");
     }
+    
+    public static void Log(string str)
+    {
+        LocalLog.LogInfo($"{MyPluginInfo.PLUGIN_GUID} {str}");
+    }
+    
+    public static void Log(string plugin_guid, string str)
+    {
+        LocalLog.LogInfo($"{plugin_guid} {str}");
+    }
 }
