87997b902b687e2bd03ce2cc7c9a1e68f6638799
Ann <ann@poppingspree.dev>
2022-10-28 23:18:04 -0500

    Fix level loading.

--- QuickBoot/QuickBootToLevel.cs
+++ QuickBoot/QuickBootToLevel.cs
@@ -13,6 +13,7 @@ namespace RisingSlash.FP2Mods.QuickBootToLevel
     {
         private ConfigEntry<string> configBootupLevel;
         private ConfigEntry<int> configSaveFileNumber;
+        private ConfigEntry<bool> configShowTransitionWipe;
 
         private bool firstUpdate = false;
         private int countUpdates = 0;
@@ -41,11 +42,17 @@ namespace RisingSlash.FP2Mods.QuickBootToLevel
 
                 string level = configBootupLevel.Value;
                 Logger.LogInfo($"Attempting to jump to BootupLevel: \"{configBootupLevel.Value}\" ");
-                if (level != null && (!level.Equals("") || level.Equals("MainMenu")))
+                if (level != null && (!level.Equals("") && !level.Equals("MainMenu")))
                 {
                     Logger.LogInfo($"Pre Boot Immediate");
-                    //BootLevelImmediate(level);
-                    BootLevel(level);
+                    if (configShowTransitionWipe.Value)
+                    {
+                        BootLevel(configBootupLevel.Value);
+                    }
+                    else
+                    {
+                        BootLevelImmediate(configBootupLevel.Value);
+                    }
                     Logger.LogInfo($"Post Boot Immediate");
                 }
                 else
@@ -92,11 +99,32 @@ namespace RisingSlash.FP2Mods.QuickBootToLevel
                 component.sceneToLoad = level;
                 FPSaveManager.menuToLoad = 2; // This is how we skip the intros.
             }
+            else
+            {
+                ConvenienceMethods.Log(MyPluginInfo.PLUGIN_GUID, "Failed to get reference to FPScreenTransition. Will not do level change.");
+            }
         }
 
         public static void BootLevelImmediate(string level)
         {
             SceneManager.LoadSceneAsync(level);
+        }
+        
+        public void PerformStageTransition()
+        {
+            try
+            {
+                var component = GameObject.Find("Screen Transition").GetComponent<FPScreenTransition>();
+                component.transitionType = FPTransitionTypes.LOCAL_WIPE;
+                component.transitionSpeed = 48f;
+                component.SetTransitionColor(0f, 0f, 0f);
+                component.BeginTransition();
+                FPAudio.PlayMenuSfx(3);
+            }
+            catch (Exception e)
+            {
+                ConvenienceMethods.LogExceptionError(e);
+            }
         }
         
         private void InitConfigs()
@@ -111,6 +139,11 @@ namespace RisingSlash.FP2Mods.QuickBootToLevel
                 "SaveNumber",  // The key of the configuration option in the configuration file
                 1, // The default value
                 "The number of the save file you wish to use when quick-booting. Set this value to 0 to boot into a level with debug settings (no save file). Normally, this would be files 1-10, but can be longer if you have additional saves (from modding or otherwise). If you need to see your save files, they are stored at \"<AppData>\\LocalLow\\GalaxyTrail\\Freedom Planet 2\"."); // Description of the option to show in the config file
+            
+            configShowTransitionWipe = Config.Bind("General",      // The section under which the option is shown
+                "ShowTransitionWipe",  // The key of the configuration option in the configuration file
+                false, // The default value
+                "Showing the black wipe transition looks cleaner and is more likely to work with built-in levels. Disable this when loading scenes from asset bundles."); // Description of the option to show in the config file
         }
     }
 }
