ef0c8ffb18cd9a037932a96e2e675735723f2713
Ann <ann@poppingspree.dev>
2022-11-14 17:11:02 -0600

    Add Timer Protector minimum time limit.

---
+++ TimerProtector/TimerProtector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using RisingSlash.FP2Mods.RisingSlashCommon;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RisingSlash.FP2Mods.TimerProtector
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("FP2.exe")]
+    public class TimerProtector : BaseUnityPlugin
+    {      
+        public void Update()
+        {
+            if (FPStage.currentStage != null)
+            {
+                EnforceTenMinuteTimerPenalty(FPStage.currentStage);
+            }
+        }
+
+        private void EnforceTenMinuteTimerPenalty(FPStage fpStage)
+        {
+            if (fpStage.minutes < 10)
+            {
+                fpStage.minutes += 10;
+            }
+        }
+    }
+}
---
+++ TimerProtector/obj/Debug/net35/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace RisingSlash.FP2Mods.TimerProtector
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "TimerProtector";
+        public const string PLUGIN_NAME = "TimerProtector";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
